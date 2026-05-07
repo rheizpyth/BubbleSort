d3b72ef4fdc34931301f33495ac87292822d48e7
Prometheus <johnrheimattdl0204@gmail.com>
2026-05-08 00:09:21 +0800

    arrayBubbleSort + Scanner Added
    Not Complete

--- BubbleSorter/BubbleS/src/MainBubble.java
+++ BubbleSorter/BubbleS/src/MainBubble.java
@@ -1,7 +1,15 @@
 import java.util.Scanner;
 
-public class MainBubble {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int [] arrayBubbleSort = new int[5];
+        System.out.println("Enter 5 numbers to sort:");
+        for (int i = 0; i < 5; i++) {
+            arrayBubbleSort[i] = scanner.nextInt();
+            
+        }
+        scanner.close();
     }
 }
