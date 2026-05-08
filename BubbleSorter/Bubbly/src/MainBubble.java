011c5e44797478ad52e8e8611312b6c12915add3
Prometheus <johnrheimattdl0204@gmail.com>
2026-05-08 14:16:11 +0800

    Little Fix

--- BubbleSorter/Bubbly/src/MainBubble.java
+++ BubbleSorter/Bubbly/src/MainBubble.java
@@ -8,8 +8,9 @@ class MainBubble {
         int n = scanner.nextInt();
         int [] arrayBubbleSort = new int[n];
         
+        System.out.println ("");
         for (int i = 0; i < arrayBubbleSort.length; i++) {
-            System.out.print("\nEnter the numbers: ");
+            System.out.print("Enter the numbers: ");
             arrayBubbleSort[i] = scanner.nextInt();
             while (arrayBubbleSort[i] < 0) {
                 System.out.print("Please enter a whole number: ");
@@ -19,9 +20,9 @@ class MainBubble {
         for (int i = 0; i < arrayBubbleSort.length - 1; i++) {
             for (int j = 0; j < arrayBubbleSort.length - i - 1; j++) {
                 if (arrayBubbleSort[j] > arrayBubbleSort[j + 1]) {
-                    int temp = arrayBubbleSort[j];
+                    int sort = arrayBubbleSort[j];
                     arrayBubbleSort[j] = arrayBubbleSort[j + 1];
-                    arrayBubbleSort[j + 1] = temp;
+                    arrayBubbleSort[j + 1] = sort;
                 }
             }
         }
